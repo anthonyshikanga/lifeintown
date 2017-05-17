dbdec149ae34c1366e9ea050cdf70fc5c2d286c1
Michael Kareithi <mikegithegi@gmail.com>
2017-05-17 13:01:06 +0300

    Search bar suggestion complete

--- find/js/scripts.js
+++ find/js/scripts.js
@@ -1,16 +1,16 @@
 //Business Logic
 
-function Place(name, coordinates, id){
+function Place(name, coordinates, id, n){
   this.name = name;
   this.coordinates = coordinates;
   this.id = id;
 }
 
-var impala = new Place("Impala Club", [-1.301459, 36.769093], "impala");
-var junction = new Place("Junction Mall", [-1.298140, 36.762118], "junction");
-var prestige = new Place("Prestige Plaza", [-1.300146, 36.787080], "prestige");
-var hub = new Place("The Hub", [-1.319509, 36.704475], "hub");
-var uhuru = new Place("Uhuru Park", [-1.289825, 36.816476], "uhuru");
+var impala = new Place("Impala Club", [-1.301459, 36.769093], "impala", 0);
+var junction = new Place("Junction Mall", [-1.298140, 36.762118], "junction", 1);
+var prestige = new Place("Prestige Plaza", [-1.300146, 36.787080], "prestige", 2);
+var hub = new Place("The Hub", [-1.319509, 36.704475], "hub", 3);
+var uhuru = new Place("Uhuru Park", [-1.289825, 36.816476], "uhuru", 4);
 
 var locations = [impala, junction, prestige, hub, uhuru];
 
@@ -30,13 +30,26 @@ $(document).ready(function() {
     event.preventDefault();
   })
 
-  locations.forEach(function(location) {
-    $("button#" + location.id).click(function() {
-      showLocation("drop-down-map", location);
-      $("#drop-down-map").toggleClass("drop-down-map-shown");
-    });
-  })
+  $(".search-bar").on('input', function() {
+    $(".search-match").remove();
+    var tester = new RegExp($(".search-bar").val(), "gi");
+    var matchesFound = [];
+    var notMatched = [];
 
+    locations.forEach(function(location) {
+      if(tester.test(location.name)) {
+        matchesFound.push(location);
+      } else {
+        notMatched.push(location);
+      }
+    })
+
+    matchesFound.forEach(function(match) {
+      $(".drop-locations").append("<div class='search-match'>"+match.name+"</div>");
+    })
+
+    });
+    //$(".drop-locations").append("<div class='search-match'>WORKING</div>");
 
   /*for(var i = 0; i < locations.length; i++) {
     alert(i)
